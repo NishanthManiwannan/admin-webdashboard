62800a27bf0e4876c13fc9de3fafbeeab0134928
Nishanth <m.nishanth1397@gmail.com>
2021-12-30 23:17:28 +0530

    route begin

--- src/App.js
+++ src/App.js
@@ -5,16 +5,24 @@ import Navbar from './components/navbar/Navbar'
 import Home from './components/pages/Home/Home';
 import Sidebar from './components/sidebar/Sidebar';
 
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom'
+import UserList from './components/pages/userlist/UserList';
+
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar></Navbar>
-
       <div className="container">
+
         <Sidebar></Sidebar>
-        <Home></Home>
+
+        <Routes>
+          <Route path="/" exact element={<Home></Home>}></Route>
+          <Route path="/userList" exact element={<UserList></UserList>}></Route>
+        </Routes>
+
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/userlist/UserList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./userList.css"
+
+function UserList() {
+    return (
+        <div className= "userList">
+            userrList
+        </div>
+    )
+}
+
+export default UserList
